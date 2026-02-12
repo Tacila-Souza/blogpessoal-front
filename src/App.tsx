ab51a745ae6663abae193768441bb46cd9d406ea
Tacila Souza <tacilasouza.contato@gmail.com>
2026-02-12 15:15:52 -0300

    pagina login + pagina cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
 // import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from "./components/footer/footer";
 import Navbar from "./components/navbar/navbar";
+import Cadastro from './pages/cadastro/Cadastro';
 import Home from './pages/home/Home';
+import Login from './pages/login/Login';
 
 function App() {
 
   return (
     <>
-    <Navbar/>
-    <Home/>
-    <Footer/>
+    <BrowserRouter>
+
+      <Navbar/>
+        <div className="min-h-[80vh]">
+
+          <Routes>
+
+            {/* <Route path='/' element={<Home/>} /> */}
+            <Route path='/home' element={<Home/>} />
+            <Route path='/cadastro' element={<Cadastro/>} />
+            <Route path="/" element={<Login />} />
+
+          </Routes>
+
+        </div>
+      <Footer/>
+      
+    </BrowserRouter>
     </>
   )
 }
